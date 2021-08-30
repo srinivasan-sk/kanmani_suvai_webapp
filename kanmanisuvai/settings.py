97a9a8fb0d2e8bd93fa4a400e1753da00fbbd465
srinivasan-sk <srinikumaran1995@gmail.com>
2021-08-30 17:21:29 +0530

    third push

--- kanmanisuvai/settings.py
+++ kanmanisuvai/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@g^m=)a5)u&0to5+*qv(fh2$qnju99$snnd&kj^shlmkgffgmc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "https://kanamanisuvai-webapp.herokuapp.com" , "localhost" , '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = [ "https://kanamanisuvai-webapp.herokuapp.com", '*']
 
 
 # Application definition
